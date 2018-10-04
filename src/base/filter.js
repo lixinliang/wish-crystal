a2f70ac03ab3adc6bbf0a9ca14b82de10a2f9819
lixinliang <393464140@qq.com>
2018-10-04 23:47:13 +0800

    add

---
+++ src/base/filter.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+
+Vue.filter('stringify', (value, size) => {
+  return JSON.stringify(value, null, size)
+})
+
+Vue.filter('ellipsis', (value, size) => {
+  return `${value}`.length > size ? '...' : value
+})
+
+Vue.filter('toFixed', (value, length) => {
+  return (+value).toFixed(length)
+})
+
+Vue.filter('slice', (value, start, end) => {
+  return `${value}`.slice(start, end)
+})
+
+Vue.filter('log', (value) => {
+  console.log(value)
+  return value
+})
