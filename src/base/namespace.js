7d46de32620f69287ad4a5bf31c299f068a3f08a
lixinliang <393464140@qq.com>
2018-10-05 00:40:01 +0800

    add

---
+++ src/base/namespace.js
@@ -0,0 +1,19 @@
+import _ from 'lodash'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
+Object.assign(window, {
+  _,
+  Vue,
+  Vuex,
+  VueRouter
+})
+
+Vue.prototype.$_ = _
+Vue.prototype.$util = window.util
+Vue.prototype.$event = window.$event
+Vue.prototype.$window = window
