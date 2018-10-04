5b60c73af37ac62a9805559ca673397cdc6768c9
lixinliang <393464140@qq.com>
2018-10-05 00:39:58 +0800

    add

---
+++ src/base/directive.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+
+// v-visible
+Vue.use({
+  install (Vue) {
+    Vue.directive('visible', (el, { value }) => {
+      el.style['visibility'] = value ? 'visible' : 'hidden'
+    })
+  }
+})
+
+// v-fade
+Vue.use({
+  install (Vue) {
+    Vue.directive('fade', (el, { value }) => {
+      el.style['opacity'] = value ? 1 : 0
+      el.style['pointer-events'] = value ? '' : 'none'
+    })
+  }
+})
+
+// v-image
+Vue.use({
+  install (Vue) {
+    Vue.directive('image', (el, { value }) => {
+      el.style['background-image'] = value ? `url(${value})` : ''
+    })
+  }
+})
+
+// v-mc
+Vue.use({
+  install (Vue) {
+    Vue.directive('mc', {
+      bind (el, { value }) {
+        const mc = new window.Hammer.Manager(el)
+        value['@mc'] = mc
+        value(mc)
+      },
+      unbind (el, { value }) {
+        const mc = value['@mc']
+        mc.destroy()
+      }
+    })
+  }
+})
