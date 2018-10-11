a669f520bd528ec5eea3880651a8ffb4bd1afb11
lixinliang <393464140@qq.com>
2018-10-11 10:21:16 +0800

    add

---
+++ src/base/index.js
@@ -0,0 +1,9 @@
+import './util'
+import './event'
+import './detect'
+import './filter'
+import './directive'
+// @depend 依赖 util.js event.js
+import './namespace'
+// @depend 依赖 namespace.js event.js
+import './i18n'
