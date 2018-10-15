4107a2ef1fc7f7af75abccc24e3d8e4f991a0923
lixinliang <393464140@qq.com>
2018-10-16 00:13:44 +0800

    update

--- src/layout/actionsheet/index.vue
+++ src/layout/actionsheet/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="layout-actionsheet">
-    actionsheet
+    <actionsheet v-model="show" :menus="menus" :cancel-text="$t('layout-actionsheet@cancel')" show-cancel @on-click-menu="click" @on-click-mask="close"></actionsheet>
   </div>
 </template>
+
+<i18n>
+layout-actionsheet@cancel:
+  en: cancel
+  zh-CN: 取消
+</i18n>
 
 <script>
 import { Actionsheet } from 'vux'
@@ -13,9 +19,25 @@ export default {
   },
   data () {
     return {
+      show: false,
+      menus: {}
     }
   },
   created () {
+    window.$event.on('app:actionsheet', ({ show, menus }) => {
+      this.show = !!show
+      this.menus = menus
+    })
+  },
+  methods: {
+    click (key) {
+      // todo
+      console.log('click', key)
+    },
+    close () {
+      // todo
+      console.log('close')
+    }
   }
 }
 </script>
