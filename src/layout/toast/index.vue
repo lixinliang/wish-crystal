e34665c4f5035d32edc99b2eb544a3c71d86081b
lixinliang <393464140@qq.com>
2018-10-16 00:13:22 +0800

    update

--- src/layout/toast/index.vue
+++ src/layout/toast/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout-toast">
-    toast
+    <toast v-for="item in list" :key="item.id" :value="true" :text="item.text" :width="item.width" :type="'text'" @on-hide="remove(item)"/>
   </div>
 </template>
 
@@ -13,9 +13,32 @@ export default {
   },
   data () {
     return {
+      list: []
     }
   },
   created () {
+    window.$event.on('app:toast', ({ text, width }) => {
+      const item = {}
+      item.text = `${text}`
+      item.width = `${width}`
+      item.id = this.$.now
+      item.disabled = false
+      this.list.push(item)
+    })
+    window.util.loop(async () => {
+      await window.util.sleep(60 * 1000)
+      if (this.list.length) {
+        const bool = window._.every(this.list, 'disabled')
+        if (bool) {
+          this.list = []
+        }
+      }
+    })
+  },
+  methods: {
+    async remove (item) {
+      item.disabled = true
+    }
   }
 }
 </script>
