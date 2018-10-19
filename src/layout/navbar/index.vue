d9c2984473bdcc2ca3df53252f9d897876f493af
lixinliang <393464140@qq.com>
2018-10-19 12:11:53 +0800

    update

--- src/layout/navbar/index.vue
+++ src/layout/navbar/index.vue
@@ -1,43 +1,59 @@
 <template>
-  <div class="layout-navbar">
-    <x-header v-if="displayHeader" :left-options="{ backText, showBack }" @on-click-title="onClickTitle">
+  <div class="layout-navbar" :class="style" v-if="this.$route.name">
+    <x-header v-if="displayHeader" :left-options="{ backText, showBack, preventGoBack }" :right-options="{ showMore }" @on-click-title="onClickTitle" @on-click-back="onClickBack" @on-click-more="onClickMore">
       <span class="title">{{$t(`layout-navbar@${$route.name}`)}}</span>
+      <span slot="left" v-if="displayCancel" class="left cancel" @click="onCancel">{{$t(`layout-navbar@cancel`)}}</span>
+      <span slot="right" v-if="displaySave" class="right save" @click="onSave">{{$t(`layout-navbar@save`)}}</span>
+      <span slot="right" v-if="displayGridOrList" class="right grid-or-list" @click="onGridOrList">
+        <em v-if="$storage.config['home:style'] === 'grid'" v-html="grid"/>
+        <em v-if="$storage.config['home:style'] === 'list'" v-html="list"/>
+      </span>
     </x-header>
+    <i18n-base></i18n-base>
+    <i18n-page></i18n-page>
   </div>
 </template>
 
-<i18n>
-layout-navbar@back:
-  en: Back
-  zh-CN: 返回
-layout-navbar@home:
-  en: Home
-  zh-CN: 首页
-layout-navbar@person:
-  en: Me
-  zh-CN: 我
-layout-navbar@about:
-  en: About
-  zh-CN: 关于
-layout-navbar@language:
-  en: Language
-  zh-CN: 选择语言
-</i18n>
-
 <script>
 import { XHeader } from 'vux'
+import grid from '@/img/grid.svg'
+import list from '@/img/list.svg'
+import i18nBase from './i18n-base'
+import i18nPage from './i18n-page'
+
+const { _ } = window
 
 export default {
   components: {
-    XHeader
+    XHeader,
+    i18nBase,
+    i18nPage
+  },
+  data () {
+    return {
+      grid,
+      list,
+      store: new Vuex.Store
+    }
   },
   computed: {
+    style () {
+      const { state, getter } = this.store
+      _.assign({}, state, getter)
+    },
     displayHeader () {
-      if (!this.$route.name) {
-        return false
-      }
       switch (this.$route.name) {
         case 'index':
+          return false
+      }
+      return true
+    },
+    showBack () {
+      switch (this.$route.name) {
+        case 'home':
+        case 'name':
+        case 'person':
+        case 'wish':
           return false
       }
       return true
@@ -45,30 +61,147 @@ export default {
     backText () {
       return this.$t('layout-navbar@back')
     },
-    showBack () {
-      if (!this.$route.name) {
-        return false
+    preventGoBack () {
+      return false
+    },
+    showMore () {
+      switch (this.$route.name) {
+        case 'photo':
+          return true
       }
+      return false
+    },
+    displayCancel () {
+      switch (this.$route.name) {
+        case 'name':
+          return true
+      }
+      return false
+    },
+    displaySave () {
+      switch (this.$route.name) {
+        case 'name':
+          return true
+      }
+      return false
+    },
+    displayGridOrList () {
       switch (this.$route.name) {
         case 'home':
-        case 'person':
-          return false
+          return true
       }
-      return true
+      return false
     }
   },
   methods: {
     onClickTitle () {
-      // todo
-      console.log('onClickTitle')
+      window.$event.emit('layout-navbar:click', 'title')
+    },
+    onClickBack () {
+      window.$event.emit('layout-navbar:click', 'back')
+    },
+    onClickMore () {
+      window.$event.emit('layout-navbar:click', 'more')
+    },
+    onCancel () {
+      window.$event.emit('layout-navbar:click', 'cancel')
+    },
+    onSave () {
+      window.$event.emit('layout-navbar:click', 'save')
+    },
+    async onGridOrList () {
+      const style = this.$storage.config['home:style']
+      let value
+      if (style === 'list') {
+        value = 'grid'
+      }
+      if (style === 'grid') {
+        value = 'list'
+      }
+      await this.$forage({ type: 'set', key: 'config@home:style', value })
     }
+  },
+  created () {
+    window.$event.on('layout-navbar:replace-state', (state) => {
+      this.store.replaceState(state)
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .title {
-    font-weight: 400;
+  @import '~@/global';
+  .layout-navbar {
+    .title {
+      font-weight: 400;
+    }
+    .cancel {
+      cursor: pointer;
+      &:active {
+        opacity: .5;
+      }
+    }
+    .save {
+      font-weight: 400;
+      color: $green-color;
+      cursor: pointer;
+      &:active {
+        opacity: .5;
+      }
+      &.is-disabled {
+        opacity: .5;
+        pointer-events: none;
+      }
+    }
+    .grid-or-list {
+      width: 20px;
+      height: 20px;
+      display: inline-block;
+      cursor: pointer;
+      &:active {
+        opacity: .5;
+      }
+      em {
+        pointer-events: none;
+      }
+    }
+  }
+</style>
+
+<style lang="scss">
+  .layout-navbar {
+    .vux-header {
+      .vux-header-left {
+        .vux-header-back {
+          &:active {
+            & + .left-arrow {
+              opacity: .5;
+            }
+          }
+        }
+        .left-arrow {
+          &::before {
+            width: 10px;
+            height: 10px;
+            top: 10px;
+          }
+        }
+      }
+      .vux-header-right {
+        .vux-header-more {
+          &::after {
+            font-size: 12px;
+          }
+        }
+        .grid-or-list {
+          svg {
+            width: 20px;
+            height: 20px;
+            display: inline-block;
+          }
+        }
+      }
+    }
   }
 </style>
 
