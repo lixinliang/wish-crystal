d09000cbc7a210438bddf23d12b6076725114491
lixinliang <393464140@qq.com>
2018-10-24 11:00:54 +0800

    update view

---
+++ src/view/wish/list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list">
+    <card v-for="(item, $index) in list" :key="$index" :item="item" :index="$index"/>
+  </div>
+</template>
+
+<script>
+import card from './card'
+
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    card
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/global';
+  .list {
+    overflow: hidden;
+    padding-bottom: 70px;
+    position: relative;
+  }
+</style>
