77f2fc378fca10f5a61b6cb83accf0c8850d0369
lixinliang <393464140@qq.com>
2018-10-24 11:00:14 +0800

    update view

---
+++ src/view/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="index">
+    <div class="logo">
+      <i v-image:size.96x96="icon"/>
+    </div>
+    <p>{{$t('index@title')}}</p>
+    <p>{{$t('index@version')}}：{{version}}.{{$timestamp|format('YYYYMMDD.HHmmss')}}</p>
+  </div>
+</template>
+
+<i18n>
+index@title:
+  en: Wish Crystal
+  zh-CN: 愿望水晶
+index@version:
+  en: Version
+  zh-CN: 版本
+</i18n>
+
+<script>
+import pkg from 'package.json'
+import icon from '@/img/logo.png'
+
+const { microApp } = window
+const { title } = document
+const { version } = pkg
+
+microApp.capable = true
+microApp.icon = icon
+microApp.title = title
+microApp.statusBarStyle = 'black-translucent'
+
+export default {
+  data () {
+    return {
+      icon,
+      version
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/global';
+  #index {
+    @include page-index;
+  }
+</style>
