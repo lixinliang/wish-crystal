9f3e7a9d5ccb004440d8e7c338e4cac6dda4c78f
lixinliang <393464140@qq.com>
2018-10-24 11:00:11 +0800

    update view

---
+++ src/view/home/list-style.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="list-style">
+    <group>
+      <cell v-for="(item, $index) in list" :key="$index" :title="$t(`home@${item.name}`)" is-link @click.native="$emit('click', item)">
+        <span slot="icon" class="symbol" v-html="item.icon"/>
+        <badge v-if="item.badge" :text="item.badge | badge"/>
+      </cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Cell, Badge, Group } from 'vux'
+
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    Cell,
+    Badge,
+    Group
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/global';
+  .list-style {
+    .weui-cell {
+      .symbol {
+        top: 3px;
+        width: 18px;
+        height: 18px;
+        display: inline-block;
+        position: relative;
+        margin-right: 8px;
+      }
+      .vux-badge {
+        display: block;
+      }
+    }
+  }
+</style>
