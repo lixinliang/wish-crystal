fe636f3e93e76c40814cf212cbba736ee59d25af
lixinliang <393464140@qq.com>
2018-10-24 11:01:10 +0800

    update widget

---
+++ src/widget/default-name/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <span class="widget-default-name"><template v-if="$storage.user.name">{{$storage.user.name}}</template><template v-else>{{$t('widget-default-name@name')}}</template></span>
+</template>
+
+<i18n>
+widget-default-name@name:
+  en: Anonymous User
+  zh-CN: 你还木有昵称呢
+</i18n>
