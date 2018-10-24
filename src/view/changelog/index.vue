e841254e073caa787d339db2bfd76049a829908c
lixinliang <393464140@qq.com>
2018-10-24 10:59:56 +0800

    update view

---
+++ src/view/changelog/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="changelog">
+    <widget-scroll-box>
+      <group v-for="(item, $key) in changelog" :key="$key" :title="$key">
+        <cell v-for="(title, $index) in item['zh-CN']" :key="$index" :title="title"></cell>
+      </group>
+    </widget-scroll-box>
+  </div>
+</template>
+
+<script>
+import { Cell, Group } from 'vux'
+import changelog from 'changelog.yml'
+import widgetScrollBox from '@/widget/scroll-box'
+
+export default {
+  components: {
+    Cell,
+    Group,
+    widgetScrollBox
+  },
+  data () {
+    return {
+      changelog
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/global';
+  #changelog {
+    @include page-base;
+  }
+</style>
