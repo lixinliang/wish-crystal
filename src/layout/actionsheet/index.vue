b695efa71430b86306cb3e616f73d8a7ca1ccb29
lixinliang <393464140@qq.com>
2018-10-24 10:53:50 +0800

    update layout

--- src/layout/actionsheet/index.vue
+++ src/layout/actionsheet/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout-actionsheet">
-    <actionsheet v-model="show" :menus="menus" :cancel-text="$t('layout-actionsheet@cancel')" show-cancel @on-click-menu="click" @on-click-mask="close"></actionsheet>
+    <actionsheet v-model="show" :menus="menus" :cancel-text="$t('layout-actionsheet@cancel')" show-cancel @on-click-menu="click" @on-click-mask="close"/>
   </div>
 </template>
 
