fec14a3f1cae80e61bea15b9f0e6ceb24fcb7c22
lixinliang <393464140@qq.com>
2018-10-25 11:07:22 +0800

    update storage

--- src/storage/config.js
+++ src/storage/config.js
@@ -1,13 +1,5 @@
 export default {
-  proto: [
-    'home:style'
-  ],
-  async init (forage) {
-    const value = await forage({ type: 'get', key: 'config@home:style' })
-    if (value === 'list' || value === 'grid') {
-      // noop
-    } else {
-      await forage({ type: 'set', key: 'config@home:style', value: 'grid' })
-    }
+  'home:style': {
+    defaultValue: 'grid'
   }
 }
