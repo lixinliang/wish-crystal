c593286d491bbcf3165efbea8ec4d8e73fbf89f5
lixinliang <393464140@qq.com>
2018-10-25 11:07:25 +0800

    update storage

--- src/storage/index.js
+++ src/storage/index.js
@@ -2,35 +2,50 @@ import _ from 'lodash'
 import Vue from 'vue'
 import pkg from 'package.json'
 import user from './user'
+import wish from './wish'
 import config from './config'
 
+const closure = {}
+const separator = '@'
 const { version } = pkg
-const [ major, minor ] = version.split('.')
-const app = `${major}.${minor}`
-
 const { $storage } = Vue.prototype
 
-const closure = {}
+const [ major, minor ] = version.split('.')
+const appVersion = `${major}.${minor}`
+
+function ellipsis (value) {
+  const string = `${value}`
+  if (string.length <= 20) {
+    return string
+  } else {
+    return `${string.slice(0, 10)}...${string.slice(-10)}`
+  }
+}
 
 const forage = async ({ type, key, value }) => {
-  const [ name, item ] = key.split('@')
-  const storage = closure[name]
-  const path = `${app}/${item}`
+  const [ storageName, keyName ] = key.split(separator)
+  const storage = closure[storageName]
   if (type === 'get') {
-    const result = await storage.getItem(path)
-    console.log(`[storage.js]@forage:type=${type};path=${path};result=${result};name=${name}`)
+    const result = await storage.getItem(keyName)
+    console.log(`[storage.js]@forage:type=${type};key=${key};result=${ellipsis(result)}`)
     return result
   }
   if (type === 'set') {
-    const result = await storage.setItem(path, value)
-    $storage[name][item] = result
-    console.log(`[storage.js]@forage:type=${type};path=${path};result=${result};name=${name}`)
+    const result = await storage.setItem(keyName, value)
+    $storage[storageName][keyName] = result
+    console.log(`[storage.js]@forage:type=${type};key=${key};result=${ellipsis(result)}`)
     return result
   }
   if (type === 'remove') {
-    $storage[name][item] = null
-    console.log(`[storage.js]@forage:type=${type};path=${path};name=${name}`)
-    return storage.removeItem(path)
+    $storage[storageName][keyName] = null
+    console.log(`[storage.js]@forage:type=${type};key=${key}`)
+    return storage.removeItem(keyName)
+  }
+  if (type === 'update') {
+    const value = $storage[storageName][keyName]
+    const result = await storage.setItem(keyName, value)
+    console.log(`[storage.js]@forage:type=${type};key=${key}`)
+    return result
   }
 }
 
@@ -39,21 +54,47 @@ Vue.prototype.$forage = forage
 export default async function () {
   await Promise.all(_.map({
     user,
+    wish,
     config
-  }, async ({ proto, init }, name) => {
-    Vue.set($storage, name, window.util.observe(_.assign({}, _(proto).keyBy((item) => item).mapValues(() => '').value())))
+  }, async (proto, storageName) => {
+    const name = `${storageName}#${appVersion}`
     const storage = window.localforage.createInstance({
       name
     })
-    closure[name] = storage
-    await Promise.all(_.map(proto, async (item) => {
-      const key = `${name}@${item}`
-      const value = await forage({
+    const entity = _.mapValues(proto, () => '')
+    const observe = window.util.observe(_.assign({}, entity))
+    Vue.set($storage, storageName, observe)
+    closure[storageName] = storage
+    await Promise.all(_.map(proto, async (config, keyName) => {
+      const key = `${storageName}${separator}${keyName}`
+      let value = await forage({
         type: 'get',
         key
       })
-      $storage[name][item] = value
+      if (!value) {
+        if (config) {
+          const { defaultValue } = config
+          if (defaultValue) {
+            if (typeof defaultValue === 'function') {
+              value = await defaultValue()
+            } else {
+              value = defaultValue
+            }
+            await forage({
+              type: 'set',
+              key,
+              value
+            })
+          }
+        }
+      }
+      $storage[storageName][keyName] = value
+      if (config) {
+        const { afterAssign } = config
+        if (afterAssign) {
+          await afterAssign(value)
+        }
+      }
     }))
-    await init(forage)
   }))
 }
