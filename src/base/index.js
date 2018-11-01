bc2a0e4c53063a7bb578f7ce34309b7cbcc86037
lixinliang <393464140@qq.com>
2018-11-01 20:25:05 +0800

    add comment

--- src/base/index.js
+++ src/base/index.js
@@ -2,10 +2,13 @@
 import './util'
 // 事件
 import './event'
+// 设备检查 环境检查
 import './detect'
+// 自定义过滤器
 import './filter'
+// 自定义指令
 import './directive'
-// @depend 依赖 util.js event.js
+// 命名空间 @depend 依赖 util.js event.js
 import './namespace'
 // @depend 依赖 namespace.js event.js
 import './i18n'
