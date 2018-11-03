563fd8c3da3aaae5069a3df911d45ebcf1a318af
lixinliang <393464140@qq.com>
2018-11-03 22:48:03 +0800

    Update src/view/wish-edit/index.vue

--- src/view/wish-edit/index.vue
+++ src/view/wish-edit/index.vue
@@ -1,41 +1,60 @@
 <template>
   <div id="wish-edit">
     <widget-scroll-box>
+      <layout-navbar-shadow/>
       <group v-if="item">
         <x-input v-model="title" :max="20" :placeholder="$t('wish-edit@title')"/>
         <x-textarea v-model="content" :max="200" :placeholder="$t('wish-edit@content')"/>
       </group>
     </widget-scroll-box>
+    <layout-navbar :title="$t('wish-edit@layout-navbar-title')" @tap="navbarTap">
+      <a slot="left">{{$t('layout-navbar@cancel')}}</a>
+      <a slot="right" class="primary" :class="{ 'disabled': disabled }">{{$t('layout-navbar@create')}}</a>
+    </layout-navbar>
   </div>
 </template>
 
 <i18n>
+wish-edit@layout-navbar-title:
+  en: Edit Wish Note
+  zh-CN: 编辑心愿贴
 wish-edit@title:
   en: Title
   zh-CN: 标题
 wish-edit@content:
   en: Content
   zh-CN: 内容
+wish-edit@can-not-edit:
+  en: Edit is disabled (✿◡‿◡)
+  zh-CN: 暂不支持编辑功能 (✿◡‿◡)
 </i18n>
 
 <script>
 import { Group, XInput, XTextarea } from 'vux'
+import layoutNavbar from '@/layout/navbar'
+import layoutNavbarShadow from '@/layout/navbar-shadow'
 import widgetScrollBox from '@/widget/scroll-box'
-
-const { _ } = window
-
-const disposable = []
 
 export default {
   components: {
     Group,
     XInput,
     XTextarea,
+    layoutNavbar,
+    layoutNavbarShadow,
     widgetScrollBox
   },
   computed: {
     item () {
       return this.$route && this.$route.params && this.$route.params.item
+    },
+    disabled () {
+      const { title, content } = this
+      if (title && content) {
+        return false
+      } else {
+        return true
+      }
     }
   },
   data () {
@@ -45,47 +64,23 @@ export default {
     }
   },
   created () {
-    console.log(this.item)
-    const toy = window.$event.listen('layout-navbar:click', async (type) => {
-      if (type === 'save') {
-        const { now } = this.$
-        const { title, content } = this
-        const { name, checkcode } = this.$storage.user
-        const key = `${now}${checkcode}`
-        const value = {
-          name,
-          title,
-          content
-        }
-        this.$set(this.$storage.wish.map, key, value)
-        await this.$forage({ type: 'update', key: 'wish@map' })
-        const text = this.$t('app@save-success')
-        window.$event.emit('app:toast', { text, width: '20em' })
-        const { item } = this
-        this.$replace('wish-detail', { item })
-      }
-      if (type === 'cancel') {
-        const { item } = this
-        this.$replace('wish-detail', { item })
-      }
-    })
-    disposable.push(toy)
-    window.$event.emit('layout-navbar:style', () => {
-      return window.util.computed({
-        'has-save-is-disabled': () => {
-          const { title, content } = this
-          if (title && content) {
-            return false
-          } else {
-            return true
-          }
-        }
-      })
-    })
+    if (this.item) {
+      this.title = this.item.title
+      this.content = this.item.content
+    }
   },
-  destroyed () {
-    _.forEach(disposable, (item) => item.remove())
-    window.$event.emit('layout-navbar:style')
+  methods: {
+    async navbarTap ({ type, preventDefault }) {
+      if (type === 'left') {
+        preventDefault()
+        const { item } = this
+        this.$replace('wish-detail', { item })
+      }
+      if (type === 'right') {
+        const content = this.$t('wish-edit@can-not-edit')
+        this.$sdk.alert({ content })
+      }
+    }
   }
 }
 </script>
@@ -94,18 +89,11 @@ export default {
   @import '~@/global';
   #wish-edit {
     @include page-base;
-  }
-</style>
-
-<style lang="scss">
-  .layout-navbar {
-    &.has-save-is-disabled {
-      .vux-header {
-        .save {
-          opacity: .5;
-          pointer-events: none;
-        }
-      }
+    .primary {
+      @include navbar-button-primary;
+    }
+    .disabled {
+      @include navbar-button-disabled;
     }
   }
 </style>
