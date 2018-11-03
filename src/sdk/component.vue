83fe5150b7e62ffdad7ad75178dc2b2a9983d8ae
lixinliang <393464140@qq.com>
2018-11-03 22:08:58 +0800

    Update src/sdk/component.vue

--- src/sdk/component.vue
+++ src/sdk/component.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="sdk">
-    <!-- <alert/> -->
+    <alert/>
     <toast/>
     <!-- <picker/> -->
     <!-- <confirm/> -->
-    <!-- <loading/> -->
+    <loading/>
     <actionsheet/>
   </div>
 </template>
 
 <script>
 import './namespace'
-// import alert from './alert'
+import alert from './alert'
 import toast from './toast'
 // import picker from './picker'
 // import confirm from './confirm'
-// import loading from './loading'
+import loading from './loading'
 import actionsheet from './actionsheet'
 
 export default {
   components: {
-    // alert,
+    alert,
     toast,
     // picker,
     // confirm,
-    // loading,
+    loading,
     actionsheet
   }
 }
