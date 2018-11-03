26f68c317985776416f4186a66f90f59be03ec74
lixinliang <393464140@qq.com>
2018-11-03 12:09:20 +0800

    update actionsheet

--- src/sdk/actionsheet/index.vue
+++ src/sdk/actionsheet/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="layout-actionsheet">
-    <actionsheet v-model="show" :menus="menus" :cancel-text="$t('layout-actionsheet@cancel')" show-cancel @on-click-menu="click" @on-click-mask="close"/>
+    <actionsheet v-model="show" :menus="menus" :cancel-text="$t('sdk-actionsheet@cancel')" show-cancel @on-click-menu="click" @on-click-mask="close"/>
   </div>
 </template>
 
 <i18n>
-layout-actionsheet@cancel:
+sdk-actionsheet@cancel:
   en: cancel
   zh-CN: 取消
 </i18n>
 
 <script>
 import { Actionsheet } from 'vux'
+
+let resolve
 
 export default {
   components: {
@@ -27,29 +29,28 @@ export default {
     window.sdk.actionsheet = async ({ menus }) => {
       this.menus = menus
       this.show = true
-      const result = await Promise.race([
-        new Promise((resolve) => window.$event.once('layout-actionsheet:click', resolve)),
-        new Promise((resolve) => window.$event.once('layout-actionsheet:close', resolve))
-      ])
+      let promise
+      ({ promise, resolve } = window.util.promise())
+      const result = await promise
       return result
     }
   },
   methods: {
     click (index) {
       if (index === 'cancel') {
-        window.$event.emit('layout-actionsheet:close', {
+        resolve({
           index: -1
         })
         return
       }
       const value = this.menus[index]
-      window.$event.emit('layout-actionsheet:click', {
+      resolve({
         index,
         value
       })
     },
     close () {
-      window.$event.emit('layout-actionsheet:close', {
+      resolve({
         index: -1
       })
     }
