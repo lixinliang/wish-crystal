ce3467f1963dd59b14c4f4467461a07b320c1baf
lixinliang <393464140@qq.com>
2018-11-03 22:09:48 +0800

    Update src/view/tree/index.vue

--- src/view/tree/index.vue
+++ src/view/tree/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="tree">
     <widget-scroll-box>
+      <layout-navbar-shadow/>
       <group :title="'dependencies'">
         <cell v-for="(value, key) in dependencies" :key="key" :title="key" :value="value"></cell>
       </group>
@@ -8,12 +9,21 @@
         <cell v-for="(value, key) in devDependencies" :key="key" :title="key" :value="value"></cell>
       </group>
     </widget-scroll-box>
+    <layout-navbar :title="$t('tree@layout-navbar-title')"/>
   </div>
 </template>
+
+<i18n>
+tree@layout-navbar-title:
+  en: Dependence Trees
+  zh-CN: 依赖树
+</i18n>
 
 <script>
 import { Cell, Group } from 'vux'
 import pkg from 'package.json'
+import layoutNavbar from '@/layout/navbar'
+import layoutNavbarShadow from '@/layout/navbar-shadow'
 import widgetScrollBox from '@/widget/scroll-box'
 
 const { dependencies, devDependencies } = pkg
@@ -22,6 +32,8 @@ export default {
   components: {
     Cell,
     Group,
+    layoutNavbar,
+    layoutNavbarShadow,
     widgetScrollBox
   },
   data () {
