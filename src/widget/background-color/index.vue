625b16b31f37daa6b56d81d47329b34138cbff01
lixinliang <393464140@qq.com>
2018-11-11 15:13:07 +0800

    Update src/widget/background-color/index.vue

---
+++ src/widget/background-color/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="widget-background-color" :style="{ 'background-color': `${color}` }">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~@/global';
+  .widget-background-color {
+    height: 100%;
+    position: relative;
+    background-color: $background-color-normal;
+  }
+</style>
