ef8fb39a0d4cca1c306fbd7bc269bd42b3a5121e
lixinliang <393464140@qq.com>
2018-11-11 16:46:03 +0800

    Update src/navigation/effect-header/index.vue

--- src/navigation/effect-header/index.vue
+++ src/navigation/effect-header/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navigation-effect-header" :style="{ 'transition': `transform ${duration}ms ease` }">
+  <div class="navigation-effect-header" :style="{ 'transition': `opacity ${duration}ms ease` }">
     <slot/>
   </div>
 </template>
@@ -15,3 +15,52 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  @mixin show {
+    opacity: 1;
+  }
+  @mixin hide {
+    opacity: 0;
+  }
+  .navigation-push-enter-start {
+    .navigation-effect-header {
+      @include hide;
+    }
+  }
+  .navigation-push-enter-end {
+    .navigation-effect-header {
+      @include show;
+    }
+  }
+  .navigation-push-leave-start {
+    .navigation-effect-header {
+      @include show;
+    }
+  }
+  .navigation-push-leave-end {
+    .navigation-effect-header {
+      @include hide;
+    }
+  }
+  .navigation-pop-enter-start {
+    .navigation-effect-header {
+      @include hide;
+    }
+  }
+  .navigation-pop-enter-end {
+    .navigation-effect-header {
+      @include show;
+    }
+  }
+  .navigation-pop-leave-start {
+    .navigation-effect-header {
+      @include show;
+    }
+  }
+  .navigation-pop-leave-end {
+    .navigation-effect-header {
+      @include hide;
+    }
+  }
+</style>
