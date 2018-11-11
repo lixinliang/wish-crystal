c2df277e2081928808a8523652c2f3e5b291e616
lixinliang <393464140@qq.com>
2018-11-11 16:46:00 +0800

    Update src/navigation/effect-box/index.vue

--- src/navigation/effect-box/index.vue
+++ src/navigation/effect-box/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="navigation-effect-box" :style="{ 'transition': `transform ${duration}ms ease` }">
+  <div class="navigation-effect-box" :style="{ 'transition': `transform ${duration}ms ease, box-shadow ${duration}ms ease` }">
     <slot/>
+    <masker :style="{ 'transition': `opacity ${duration}ms ease` }" :opacity=".1"/>
   </div>
 </template>
 
 <script>
+import { Masker } from 'vux'
 import duration from '../duration'
 
 export default {
+  components: {
+    Masker
+  },
   data () {
     return {
       duration
@@ -16,10 +21,19 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .navigation-effect-box {
     height: 100%;
     position: relative;
+  }
+  .vux-masker-box {
+    top: 0;
+    left: 0;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    pointer-events: none;
   }
 </style>
 
@@ -33,44 +47,88 @@ export default {
   @mixin right {
     transform: translate3d(100% ,0, 0);
   }
+  @mixin show-mask {
+    opacity: 1;
+  }
+  @mixin hide-mask {
+    opacity: 0;
+  }
+  @mixin show-shadow {
+    box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, .6);
+  }
+  @mixin hide-shadow {
+    box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0);
+  }
   .navigation-push-enter-start {
     .navigation-effect-box {
       @include right;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
   .navigation-push-enter-end {
     .navigation-effect-box {
       @include center;
+      @include show-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
   .navigation-push-leave-start {
     .navigation-effect-box {
       @include center;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
   .navigation-push-leave-end {
     .navigation-effect-box {
       @include left;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include show-mask;
     }
   }
   .navigation-pop-enter-start {
     .navigation-effect-box {
       @include left;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include show-mask;
     }
   }
   .navigation-pop-enter-end {
     .navigation-effect-box {
       @include center;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
   .navigation-pop-leave-start {
     .navigation-effect-box {
       @include center;
+      @include show-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
   .navigation-pop-leave-end {
     .navigation-effect-box {
       @include right;
+      @include hide-shadow;
+    }
+    .vux-masker-box {
+      @include hide-mask;
     }
   }
 </style>
