cba8309b22aad1e2b001fa1682ffff45256d34e8
lixinliang <393464140@qq.com>
2018-11-11 17:47:48 +0800

    Update src/view/name/index.vue

--- src/view/name/index.vue
+++ src/view/name/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="name">
-    <widget-scroll-box>
-      <layout-navbar-shadow/>
-      <group>
-        <x-input :max="20" v-model="value"/>
-      </group>
-    </widget-scroll-box>
+    <navigation-effect-box>
+      <widget-background-color>
+        <widget-scroll-box>
+          <layout-navbar-shadow/>
+          <group>
+            <x-input :max="20" v-model="value"/>
+          </group>
+        </widget-scroll-box>
+      </widget-background-color>
+    </navigation-effect-box>
+    <layout-navbar-color/>
     <layout-navbar :title="$t('name@layout-navbar-title')" @tap="navbarTap">
       <a slot="left">{{$t('layout-navbar@cancel')}}</a>
       <a slot="right" class="primary" :class="{ 'disabled': disabled }">{{$t('layout-navbar@save')}}</a>
@@ -22,16 +27,22 @@ name@layout-navbar-title:
 <script>
 import { Group, XInput } from 'vux'
 import layoutNavbar from '@/layout/navbar'
+import layoutNavbarColor from '@/layout/navbar-color'
 import layoutNavbarShadow from '@/layout/navbar-shadow'
 import widgetScrollBox from '@/widget/scroll-box'
+import widgetBackgroundColor from '@/widget/background-color'
+import navigationEffectBox from '@/navigation/effect-box'
 
 export default {
   components: {
     Group,
     XInput,
     layoutNavbar,
+    layoutNavbarColor,
     layoutNavbarShadow,
-    widgetScrollBox
+    widgetScrollBox,
+    widgetBackgroundColor,
+    navigationEffectBox
   },
   data () {
     return {
