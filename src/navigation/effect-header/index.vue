306716709eabf13becfa49a2f6c27728d03fe279
lixinliang <393464140@qq.com>
2018-11-11 15:12:47 +0800

    Update src/navigation/effect-header/index.vue

---
+++ src/navigation/effect-header/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navigation-effect-header" :style="{ 'transition': `transform ${duration}ms ease` }">
+    <slot/>
+  </div>
+</template>
+
+<script>
+import duration from '../duration'
+
+export default {
+  data () {
+    return {
+      duration
+    }
+  }
+}
+</script>
