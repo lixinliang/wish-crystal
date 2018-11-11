5546d0efc60163c5f345bbe4c0f23b1b1ea7dfff
lixinliang <393464140@qq.com>
2018-11-11 13:44:53 +0800

    Update src/base/detect.js

--- src/base/detect.js
+++ src/base/detect.js
@@ -2,6 +2,7 @@ const html = document.documentElement
 const { userAgent, standalone, onLine } = window.navigator
 const { width, height } = window.screen
 
+// 设备检查
 if (/android/.test(userAgent)) {
   html.classList.add('android')
 } else {
@@ -18,22 +19,28 @@ html.classList.add(`width${width}`)
 html.classList.add(`height${height}`)
 html.classList.add(`dpi${devicePixelRatio}`)
 
+// PWA
 if (standalone) {
   html.classList.add('standalone')
 } else {
   html.classList.add('browser')
 }
 
+// 离线应用
 if (onLine) {
   html.classList.add('online')
 } else {
   html.classList.add('offline')
 }
 
+// 开发环境
 if (process.env.NODE_ENV === 'development') {
   html.classList.add('development')
+  // 模拟 PWA
+  html.classList.add('standalone')
 }
 
+// 生产环境
 if (process.env.NODE_ENV === 'production') {
   html.classList.add('production')
 }
