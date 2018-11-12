1656824228f8f9749c999675381295d4f9ebe99f
lixinliang <393464140@qq.com>
2018-11-12 12:34:03 +0800

    Update src/sdk/masker/index.vue

---
+++ src/sdk/masker/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sdk-masker">
+    <masker v-if="show" :color="color" :opacity="opacity" :fullscreen="fullscreen"/>
+  </div>
+</template>
+
+<script>
+import { Masker } from 'vux'
+
+export default {
+  components: {
+    Masker
+  },
+  data () {
+    return {
+      show: false,
+      color: '',
+      opacity: '',
+      fullscreen: false
+    }
+  },
+  created () {
+    window.sdk.masker = ({ show, color, opacity, fullscreen }) => {
+      this.show = !!show
+      this.color = `${color}`
+      this.opacity = +opacity || 0
+      this.fullscreen = !!fullscreen
+    }
+  }
+}
+</script>
