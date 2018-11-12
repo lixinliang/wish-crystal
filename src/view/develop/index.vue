6f0d405b8ea9a40f6e6d13aab2c6a2ff8a3a0b39
lixinliang <393464140@qq.com>
2018-11-12 10:06:22 +0800

    Update src/view/develop/index.vue

--- src/view/develop/index.vue
+++ src/view/develop/index.vue
@@ -10,6 +10,12 @@
           <group>
             <panel type="2" :list="list"/>
           </group>
+          <div class="button">
+            <x-button type="warn" @click.native="reboot">{{$t('develop@reboot')}}</x-button>
+          </div>
+          <div class="button">
+            <x-button type="warn" @click.native="reload">{{$t('develop@reload')}}</x-button>
+          </div>
         </widget-scroll-box>
       </widget-background-color>
     </navigation-effect-box>
@@ -25,10 +31,16 @@ develop@layout-navbar-title:
 develop@checkcode:
   en: Check Code
   zh-CN: 校验码
+develop@reboot:
+  en: Reboot
+  zh-CN: 软重启
+develop@reload:
+  en: Reload
+  zh-CN: 硬重启
 </i18n>
 
 <script>
-import { Cell, Group, Panel } from 'vux'
+import { Cell, Group, Panel, XButton } from 'vux'
 import layoutNavbar from '@/layout/navbar'
 import layoutNavbarColor from '@/layout/navbar-color'
 import layoutNavbarShadow from '@/layout/navbar-shadow'
@@ -41,6 +53,7 @@ export default {
     Cell,
     Group,
     Panel,
+    XButton,
     layoutNavbar,
     layoutNavbarColor,
     layoutNavbarShadow,
@@ -61,6 +74,32 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    reboot () {
+      window.$event.emit('root:reboot')
+    },
+    async reload () {
+      const source = 2849184197
+      const url_long = `${window.URI().hash('')}`
+      this.$sdk.loading({ show: true })
+      const response = await window.util.jsonp('http://api.weibo.com/2/short_url/shorten.json', {
+        source,
+        url_long
+      })
+      this.$sdk.loading({ show: false })
+      if (!response) {
+        this.$sdk.toast({ text: 'error' })
+        return
+      }
+      const { code, data } = response
+      if (!code || !data) {
+        this.$sdk.toast({ text: 'error' })
+        return
+      }
+      const url = data.urls[0].url_short
+      location.href = url
+    }
   }
 }
 </script>
@@ -69,6 +108,10 @@ export default {
   @import '~@/global';
   #develop {
     @include page-base;
+    .button {
+      margin-top: 20px;
+      padding: 0 15px;
+    }
   }
 </style>
 
