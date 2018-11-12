f1ec08c531e1d6b1be297c8fc1ee6fd6e86b49e0
lixinliang <393464140@qq.com>
2018-11-12 12:10:33 +0800

    Update src/base/filter.js

--- src/base/filter.js
+++ src/base/filter.js
@@ -1,30 +1,37 @@
 import Vue from 'vue'
 
+// json string
 Vue.filter('stringify', (value, size) => {
   return JSON.stringify(value, null, size)
 })
 
+// string ellipsis
 Vue.filter('ellipsis', (value, size) => {
   return `${value}`.length > size ? '...' : value
 })
 
+// number toFixed
 Vue.filter('toFixed', (value, length) => {
   return (+value).toFixed(length)
 })
 
+// string slice
 Vue.filter('slice', (value, start, end) => {
   return `${value}`.slice(start, end)
 })
 
+// log pipe
 Vue.filter('log', (value) => {
   console.log(value)
   return value
 })
 
+// format moment
 Vue.filter('format', (value, expression) => {
   return window.moment(value).format(expression)
 })
 
+// badge number
 Vue.filter('badge', (value) => {
   const badge = +value || 0
   if (badge <= 0) {
@@ -36,6 +43,7 @@ Vue.filter('badge', (value) => {
   return `${badge}`
 })
 
+// decodeURIComponent
 Vue.filter('decodeURIComponent', (value) => {
   return decodeURIComponent(`${value}`)
 })
