b29ab13753e34d4234492081d8de90d3eed2ae20
lixinliang <393464140@qq.com>
2018-11-13 09:47:25 +0800

    Update src/sdk/component.vue

--- src/sdk/component.vue
+++ src/sdk/component.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="sdk" @touchmove.prevent>
-    <alert/>
-    <toast/>
-    <masker/>
-    <!-- <picker/> -->
-    <!-- <confirm/> -->
-    <loading/>
-    <actionsheet/>
+    <alert ref="alert"/>
+    <toast ref="toast"/>
+    <masker ref="masker"/>
+    <picker ref="picker"/>
+    <confirm ref="confirm"/>
+    <loading ref="loading"/>
+    <actionsheet ref="actionsheet"/>
   </div>
 </template>
 
@@ -15,8 +15,8 @@ import './namespace'
 import alert from './alert'
 import toast from './toast'
 import masker from './masker'
-// import picker from './picker'
-// import confirm from './confirm'
+import picker from './picker'
+import confirm from './confirm'
 import loading from './loading'
 import actionsheet from './actionsheet'
 
@@ -25,8 +25,8 @@ export default {
     alert,
     toast,
     masker,
-    // picker,
-    // confirm,
+    picker,
+    confirm,
     loading,
     actionsheet
   }
