2523894a661068a3400ac2c303b5eed765ae3d95
lixinliang <393464140@qq.com>
2018-11-15 14:36:55 +0800

    Update src/view/lab/index.vue

--- src/view/lab/index.vue
+++ src/view/lab/index.vue
@@ -11,8 +11,16 @@
             <cell :title="$t('lab@splash')" is-link @click.native="$replace('splash')"></cell>
           </group>
           <group>
+            <cell :title="$t('lab@test-native')" is-link @click.native="$push('test-native')"></cell>
+          </group>
+          <group>
+            <group-title slot="title">{{$t('lab@test')}}</group-title>
             <cell :title="$t('lab@test-api')" is-link @click.native="$push('test-api')"></cell>
             <cell :title="$t('lab@test-sdk')" is-link @click.native="$push('test-sdk')"></cell>
+          </group>
+          <group>
+            <group-title slot="title">{{$t('lab@third-party-app')}}</group-title>
+            <cell v-for="(item, $index) in website" :key="$index" :title="item" is-link @click.native="$push('webview', { item })"></cell>
           </group>
         </widget-scroll-box>
       </widget-background-color>
@@ -32,10 +40,22 @@ lab@sample:
 lab@splash:
   en: Splash
   zh-CN: 启屏动画
+lab@test:
+  en: Unit Testing
+  zh-CN: 单元测试
+lab@test-api:
+  en: API Unit Testing
+  zh-CN: API 单元测试
+lab@test-sdk:
+  en: SDK Unit Testing
+  zh-CN: SDK 单元测试
+lab@third-party-app:
+  en: Third Party App
+  zh-CN: 第三方应用
 </i18n>
 
 <script>
-import { Cell, Group } from 'vux'
+import { Cell, Group, GroupTitle } from 'vux'
 import layoutNavbar from '@/layout/navbar'
 import layoutNavbarColor from '@/layout/navbar-color'
 import layoutNavbarShadow from '@/layout/navbar-shadow'
@@ -47,12 +67,22 @@ export default {
   components: {
     Cell,
     Group,
+    GroupTitle,
     layoutNavbar,
     layoutNavbarColor,
     layoutNavbarShadow,
     widgetScrollBox,
     widgetBackgroundColor,
     navigationEffectBox
+  },
+  data () {
+    return {
+      website: [
+        'yy.com',
+        'qq.com',
+        'baidu.com'
+      ]
+    }
   }
 }
 </script>
