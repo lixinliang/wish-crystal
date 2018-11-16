53dcb6f5ec14cd8d3f527ad85aa1a0569e72d258
lixinliang <393464140@qq.com>
2018-11-16 09:39:25 +0800

    Update src/view/test-sdk/index.vue

--- src/view/test-sdk/index.vue
+++ src/view/test-sdk/index.vue
@@ -6,38 +6,35 @@
           <layout-navbar-shadow/>
           <group>
             <group-title slot="title">Action Sheet</group-title>
-            <cell :title="$t('test-sdk@actionsheet-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@actionsheet-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="actionsheet(1)">{{status['actionsheet1']}}</cell>
           </group>
           <group>
             <group-title slot="title">Alert</group-title>
-            <cell :title="$t('test-sdk@alert-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@alert-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="alert(1)">{{status['alert1']}}</cell>
+            <cell :title="'Case 2'" is-link @click.native="alert(2)">{{status['alert2']}}</cell>
           </group>
           <group>
             <group-title slot="title">Confirm</group-title>
-            <cell :title="$t('test-sdk@confirm-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@confirm-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="confirm(1)">{{status['confirm1']}}</cell>
+            <cell :title="'Case 2'" is-link @click.native="confirm(2)">{{status['confirm2']}}</cell>
           </group>
           <group>
             <group-title slot="title">Loading</group-title>
-            <cell :title="$t('test-sdk@loading-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@loading-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="loading(1)">{{status['loading1']}}</cell>
+            <cell :title="'Case 2'" is-link @click.native="loading(2)">{{status['loading2']}}</cell>
           </group>
           <group>
             <group-title slot="title">Masker</group-title>
-            <cell :title="$t('test-sdk@masker-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@masker-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="masker(1)">{{status['masker1']}}</cell>
           </group>
           <group>
             <group-title slot="title">Picker</group-title>
-            <cell :title="$t('test-sdk@picker-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@picker-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="picker(1)">{{status['picker1']}}</cell>
+            <cell :title="'Case 2'" is-link @click.native="picker(2)">{{status['picker2']}}</cell>
           </group>
           <group>
             <group-title slot="title">Toast</group-title>
-            <cell :title="$t('test-sdk@toast-1')" is-link @click.native="click"></cell>
-            <cell :title="$t('test-sdk@toast-2')" is-link @click.native="click"></cell>
+            <cell :title="'Case 1'" is-link @click.native="toast(1)"/>
           </group>
         </widget-scroll-box>
       </widget-background-color>
@@ -74,10 +71,99 @@ export default {
     widgetBackgroundColor,
     navigationEffectBox
   },
-  methods: {
-    click () {
-
+  data () {
+    return {
+      status: {}
     }
+  },
+  methods: {
+    async actionsheet (index) {
+      const key = `actionsheet${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        result = await this.$sdk.actionsheet({ menus: ['a', 'b', 'c'] })
+      }
+      this.$set(this.status, key, result)
+    },
+    async alert (index) {
+      const key = `alert${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        await this.$sdk.alert({ content: 'content' })
+      }
+      if (index === 2) {
+        await this.$sdk.alert({ title: 'title', content: 'content' })
+      }
+      this.$set(this.status, key, result)
+    },
+    async confirm (index) {
+      const key = `confirm${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        result = await this.$sdk.confirm({ content: 'content' })
+      }
+      if (index === 2) {
+        result = await this.$sdk.confirm({ title: 'title', content: 'content' })
+      }
+      this.$set(this.status, key, result)
+    },
+    async loading (index) {
+      const key = `loading${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        this.$sdk.loading({ show: true })
+        await window.util.sleep(2000)
+        this.$sdk.loading({ show: false })
+      }
+      if (index === 2) {
+        await new Promise((resolve) => {
+          let text = 0;
+          const $break = resolve
+          window.util.loop(() => (new Promise(async (resolve) => {
+            if (text >= 100) {
+              $break()
+              return
+            }
+            this.$sdk.loading({ show: true, text })
+            text++
+            await window.util.sleep(20)
+            resolve()
+          })))
+        })
+        this.$sdk.loading({ show: false })
+      }
+      this.$set(this.status, key, result)
+    },
+    async masker (index) {
+      const key = `masker${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        this.$sdk.masker({ show: true, opacity: 0.5, fullscreen: true })
+        await window.util.sleep(2000)
+        this.$sdk.masker({ show: false, opacity: 0.5 })
+      }
+      this.$set(this.status, key, result)
+    },
+    async picker (index) {
+      const key = `picker${index}`
+      this.$set(this.status, key, 'pending')
+      let result = 'resolve'
+      if (index === 1) {
+        result = await this.$sdk.picker({ title: 'title', data: [['1', '2', '3']] })
+      }
+      if (index === 2) {
+        result = await this.$sdk.picker({ title: 'title', data: [['1', '2', '3'], ['a', 'b', 'c']] })
+      }
+      this.$set(this.status, key, result)
+    },
+    toast (index) {
+      this.$sdk.toast({ text: 'toast' })
+    },
   }
 }
 </script>
