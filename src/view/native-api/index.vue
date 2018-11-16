8fc1fa9bef1669440b189b3fe34486ed5b7968ea
lixinliang <393464140@qq.com>
2018-11-16 10:45:12 +0800

    Update src/view/native-api/index.vue

--- src/view/native-api/index.vue
+++ src/view/native-api/index.vue
@@ -1,25 +1,39 @@
 <template>
-  <div id="sample">
+  <div id="native-api">
     <navigation-effect-box>
       <widget-background-color>
         <widget-scroll-box>
           <layout-navbar-shadow/>
-          <h1>{{$t('sample@layout-navbar-title')}}</h1>
+          <group>
+            <cell :title="'Alert'" is-link @click.native="alert = $window.alert('alert')">{{alert}}</cell>
+            <cell :title="'Confirm'" is-link @click.native="confirm = $window.confirm('confirm')">{{confirm}}</cell>
+            <cell :title="'Prompt'" is-link @click.native="prompt = $window.prompt('prompt')">{{prompt}}</cell>
+          </group>
+          <group>
+            <cell :title="'Worker'"/>
+          </group>
+          <group>
+            <cell :title="'Navigator'"/>
+          </group>
+          <group>
+            <cell :title="'Performance'"/>
+          </group>
         </widget-scroll-box>
       </widget-background-color>
     </navigation-effect-box>
     <layout-navbar-color/>
-    <layout-navbar :title="$t('sample@layout-navbar-title')"/>
+    <layout-navbar :title="$t('native-api@layout-navbar-title')"/>
   </div>
 </template>
 
 <i18n>
-sample@layout-navbar-title:
-  en: Sample
-  zh-CN: 示例
+native-api@layout-navbar-title:
+  en: System Interface
+  zh-CN: 系统接口
 </i18n>
 
 <script>
+import { Cell, Group, GroupTitle } from 'vux'
 import layoutNavbar from '@/layout/navbar'
 import layoutNavbarColor from '@/layout/navbar-color'
 import layoutNavbarShadow from '@/layout/navbar-shadow'
@@ -29,19 +43,29 @@ import navigationEffectBox from '@/navigation/effect-box'
 
 export default {
   components: {
+    Cell,
+    Group,
+    GroupTitle,
     layoutNavbar,
     layoutNavbarColor,
     layoutNavbarShadow,
     widgetScrollBox,
     widgetBackgroundColor,
     navigationEffectBox
+  },
+  data () {
+    return {
+      alert: '',
+      confirm: '',
+      prompt: ''
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   @import '~@/global';
-  #sample {
+  #native-api {
     @include page-base;
   }
 </style>
